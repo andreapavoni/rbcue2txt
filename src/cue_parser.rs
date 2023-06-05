367bf975dcc899c6c9a307617a4d2f8a19313e84
Andrea Pavoni <andrea.pavoni@gmail.com>
2023-06-05 17:07:39 +0200

    small cleanups

--- src/cue_parser.rs
+++ src/cue_parser.rs
@@ -30,7 +30,7 @@ pub fn run(cue_file_path: &str) -> Result<Vec<TrackInfo>, Box<dyn error::Error>>
         .captures_iter(&file_content)
         .map(|captures| captures.get(1).unwrap().as_str())
         .collect();
-    let indices: Vec<_> = index_re
+    let timestamps: Vec<_> = index_re
         .captures_iter(&file_content)
         .map(|captures| captures.get(1).unwrap().as_str())
         .collect();
@@ -38,14 +38,10 @@ pub fn run(cue_file_path: &str) -> Result<Vec<TrackInfo>, Box<dyn error::Error>>
     let mut outputs: Vec<TrackInfo> = Vec::new();
 
     for i in 0..titles.len() {
-        let title = titles[i];
-        let artist = artists[i];
-        let time = indices[i];
-
         outputs.push(TrackInfo {
-            title: title.to_string(),
-            artist: artist.to_string(),
-            time: time.to_string(),
+            title: titles[i].to_string(),
+            artist: artists[i].to_string(),
+            time: timestamps[i].to_string(),
         });
     }
 
@@ -63,23 +59,23 @@ mod tests {
         let result = run(cue_file_path);
         assert!(result.is_ok());
 
-        let track_info = result.unwrap();
+        let playlist = result.unwrap();
 
-        assert_eq!(track_info.len(), 17);
+        assert_eq!(playlist.len(), 17);
 
         // first track
-        assert_eq!(track_info[0].title, "Shot You Down (Original Mix)");
-        assert_eq!(track_info[0].artist, "Giuseppe Surace");
-        assert_eq!(track_info[0].time, "00:00:02");
+        assert_eq!(playlist[0].title, "Shot You Down (Original Mix)");
+        assert_eq!(playlist[0].artist, "Giuseppe Surace");
+        assert_eq!(playlist[0].time, "00:00:02");
 
         // second track
-        assert_eq!(track_info[1].title, "Tzu-Mani");
-        assert_eq!(track_info[1].artist, "Aldo Cadiz");
-        assert_eq!(track_info[1].time, "00:03:06");
+        assert_eq!(playlist[1].title, "Tzu-Mani");
+        assert_eq!(playlist[1].artist, "Aldo Cadiz");
+        assert_eq!(playlist[1].time, "00:03:06");
 
         // last track
-        assert_eq!(track_info[16].title, "Super Flu 3 < Isaac");
-        assert_eq!(track_info[16].artist, "Super Flu");
-        assert_eq!(track_info[16].time, "00:57:02");
+        assert_eq!(playlist[16].title, "Super Flu 3 < Isaac");
+        assert_eq!(playlist[16].artist, "Super Flu");
+        assert_eq!(playlist[16].time, "00:57:02");
     }
 }
